e6bdfbcca976ed9a5cb41ab32a8eb86e600ab22e
Massab Iftikhar <massabiftkhar@gmail.com>
2022-12-30 21:24:11 +0500

    secure

--- controllers/managersauthController.js
+++ controllers/managersauthController.js
@@ -41,6 +41,7 @@ const signinUser = (user, statuscode, res) => {
       Date.now() + process.env.EXPIRES_COOKIE_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    sameSite: 'none',
     secure: true,
   });
 
