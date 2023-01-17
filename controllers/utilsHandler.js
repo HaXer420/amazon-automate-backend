5b95aaecdaa0410d63c50ca9aad6357a13bdb68a
Massab Iftikhar <massabiftkhar@gmail.com>
2023-01-17 19:56:23 +0500

    totals optimized

--- controllers/utilsHandler.js
+++ controllers/utilsHandler.js
@@ -11,22 +11,50 @@ const AppError = require('../utils/appError');
 
 exports.totalproductsclientsmanagersspecialists = catchAsync(
   async (req, res, next) => {
-    const totalclients = await Client.find();
-    const totalproducts = await Product.find({ status: { $ne: 'Rejected' } });
-    const unassignedproducts = await Product.find({
-      $and: [{ isApproved: { $eq: true } }, { isAssigned: { $eq: false } }],
-    });
-    const assignedproducts = await Product.find({
-      isAssigned: { $eq: true },
-    });
-    const approvedproducts = await Product.find({
-      status: { $eq: 'Approved' },
-    });
-    const pendingproducts = await Product.find({ status: { $eq: 'Pending' } });
-    const sourcingmanagers = await Manager.find({ role: { $eq: 'Sourcing' } });
-    const accountmanagers = await Manager.find({ role: { $eq: 'Account' } });
-    const specialists = await Specialist.find();
-    const sources = await Source.find();
+    async (req, res, next) => {
+      const [
+        totalclients,
+        totalproducts,
+        unassignedproducts,
+        assignedproducts,
+        approvedproducts,
+        pendingproducts,
+        sourcingmanagers,
+        accountmanagers,
+        specialists,
+        sources,
+      ] = await Promise.all([
+        Client.find(),
+        Product.find({ status: { $ne: 'Rejected' } }),
+        Product.find({
+          $and: [{ isApproved: { $eq: true } }, { isAssigned: { $eq: false } }],
+        }),
+        Product.find({ isAssigned: { $eq: true } }),
+        Product.find({ status: { $eq: 'Approved' } }),
+        Product.find({ status: { $eq: 'Pending' } }),
+        Manager.find({ role: { $eq: 'Sourcing' } }),
+        Manager.find({ role: { $eq: 'Account' } }),
+        Specialist.find(),
+        Source.find(),
+      ]);
+    };
+
+    // const totalclients = await Client.find();
+    // const totalproducts = await Product.find({ status: { $ne: 'Rejected' } });
+    // const unassignedproducts = await Product.find({
+    //   $and: [{ isApproved: { $eq: true } }, { isAssigned: { $eq: false } }],
+    // });
+    // const assignedproducts = await Product.find({
+    //   isAssigned: { $eq: true },
+    // });
+    // const approvedproducts = await Product.find({
+    //   status: { $eq: 'Approved' },
+    // });
+    // const pendingproducts = await Product.find({ status: { $eq: 'Pending' } });
+    // const sourcingmanagers = await Manager.find({ role: { $eq: 'Sourcing' } });
+    // const accountmanagers = await Manager.find({ role: { $eq: 'Account' } });
+    // const specialists = await Specialist.find();
+    // const sources = await Source.find();
 
     res.status(200).json({
       totalclients: totalclients.length,
